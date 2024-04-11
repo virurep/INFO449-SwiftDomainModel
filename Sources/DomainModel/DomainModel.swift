22f9d27b0a60496dbcdd8c03a39d5c083b2a4a93
virurepalle <virurep@uw.edu>
2024-04-10 21:53:02 -0700

    finished assignment

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -21,24 +21,48 @@ public struct Money {
         self.currency = currency
     }
     
-    func convert(_ targetCurrency:String) -> Money {
+    func convert(_ newCurrency:String) -> Money {
         
-        let usdAmount = convertUSD().amount
-        
-        switch targetCurrency {
-        case "USD" :
-            return Money(amount: usdAmount * 1, currency: targetCurrency)
-        case "GBP" :
-            return Money(amount: usdAmount * (1/2), currency: targetCurrency)
-        case "EUR" :
-            return Money(amount: usdAmount * (3/2), currency: targetCurrency)
-        case "CAN" :
-            return Money(amount: usdAmount * (5/4), currency: targetCurrency)
-        default:
-            fatalError("Invalid currency provided: \(targetCurrency)")
-        }
+        var usdAmount: Int
+                
+                switch currency {
+                case "USD":
+                    usdAmount = amount
+                    
+                case "GBP":
+                    usdAmount = Int(Double(amount) / 0.5)
+                    
+                case "EUR":
+                    usdAmount = Int(Double(amount) / 1.5)
+                    
+                case "CAN":
+                    usdAmount = Int(Double(amount) / 1.25)
+                    
+                default:
+                    fatalError("Unknown currency: \(currency)")
+                    
+                }
+                
+                
+                switch newCurrency {
+                case "USD":
+                    return Money(amount: usdAmount, currency: newCurrency)
+                    
+                case "GBP":
+                    return Money(amount: Int(Double(usdAmount) * 0.5), currency: newCurrency)
+                    
+                case "EUR":
+                    return Money(amount: Int(Double(usdAmount) * 1.5), currency: newCurrency)
+                    
+                case "CAN":
+                    return Money(amount: Int(Double(usdAmount) * 1.25), currency: newCurrency)
+                    
+                default:
+                    fatalError("Unknown currency: \(newCurrency)")
+                }
 
     }
+    
     
     func add(_ delta:Money) -> Money {
         
@@ -64,22 +88,6 @@ public struct Money {
         
     }
     
-    private func convertUSD() -> Money {
-        
-        switch currency {
-        case "GBP" :
-            return Money(amount: amount * 2, currency: "USD")
-        case "EUR" :
-            return Money(amount: amount * (2/3), currency: "USD")
-        case "CAN" :
-            return Money(amount: amount * (4/5), currency: "USD")
-        case "USD" :
-            return self
-        default:
-            fatalError("Invalid currency provided: \(currency)")
-            
-        }
-    }
     
 }
 
@@ -113,27 +121,23 @@ public class Job {
         
     }
     
-    func raise(byAmount delta:Double){
-            
+    func raise(byAmount amount: Double) {
             switch type {
-            case .Hourly(let hourlyWage):
-                type = .Hourly(hourlyWage + Double(delta))
-            case .Salary(let yearlySalary):
-                type = .Salary(yearlySalary + UInt(delta))
+            case .Hourly(let wage):
+                type = .Hourly(wage + Double(amount))
+            case .Salary(let salary):
+                type = .Salary(salary + UInt(amount))
             }
-            
-    }
-    
-    func raise(byPercent delta:Double){
-            
+        }
+        
+        func raise(byPercent percentage: Double) {
             switch type {
-            case .Hourly(let hourlyWage):
-                type = .Hourly(hourlyWage * (1.0 + delta))
-            case .Salary(let yearlySalary):
-                type = .Salary(yearlySalary + UInt(1.0 + delta))
+            case .Hourly(let wage):
+                type = .Hourly(wage * (1 + percentage))
+            case .Salary(let salary):
+                type = .Salary(UInt(Double(salary) * (1 + percentage)))
             }
-            
-    }
+        }
         
 }
 
@@ -141,10 +145,97 @@ public class Job {
 // Person
 //
 public class Person {
+    
+    let firstName:String
+    let lastName:String
+    let age:Int
+    var job: Job? {
+          didSet {
+              if age < 16 {
+                  job = nil
+              }
+          }
+      }
+      var spouse: Person? {
+          didSet {
+              if age < 16 {
+                  spouse = nil
+              }
+          }
+      }
+    
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+    
+    func toString() -> String {
+        
+            var result = "[Person: firstName:" + firstName + " lastName:" + lastName + " age:" + String(age) + " job:"
+            if job != nil {
+                result += job!.title
+            } else {
+                result += "nil"
+            }
+            result += " spouse:"
+            if spouse != nil {
+                result += spouse!.firstName
+            } else {
+                result += "nil"
+            }
+            result += "]"
+            return result
+        
+        }
+    
 }
 
 ////////////////////////////////////
 // Family
 //
 public class Family {
+    
+    var members: [Person]
+    
+    init(spouse1:Person, spouse2:Person) {
+        
+        guard spouse1.spouse == nil && spouse2.spouse == nil else {
+            fatalError("Each person can only be part of one family")
+        }
+        
+        spouse1.spouse = spouse2
+        spouse2.spouse = spouse1
+        
+        self.members = [spouse1, spouse2]
+        
+    }
+    
+    func haveChild(_ kid:Person) -> Bool {
+        
+        if members[0].age > 21 || members[1].age > 21 {
+            
+            members.append(kid)
+            return true
+        }
+        else {
+            return false
+        }
+        
+    }
+    
+    func householdIncome() -> Int {
+        
+        var totalIncome = 0
+        
+        for member in members {
+            if (member.job !== nil){
+                totalIncome += member.job!.calculateIncome(2000)
+            }
+        }
+        
+        return totalIncome
+        
+    }
+    
 }
